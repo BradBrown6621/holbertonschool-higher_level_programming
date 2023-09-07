604d1be87f4cac8fd2f620013cc0346cab2176de
BradBrown6621 <6621@holbertonstudents.com>
2023-09-07 10:05:17 -0700

    Fixed wording on 0-

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz", end=" ")
-    elif i % 3 == 0:
-        print("Fizz", end=" ")
-    elif i % 5 == 0:
-        print("Buzz", end=" ")
-    else:
-        print(i, end=" ")
+import random
+number = random.randint(-10, 10)
+polarity = ""
+if (number > 0):
+    polarity = "positive"
+elif (number < 0):
+    polarity = "negative"
+else:
+    polarity = "zero"
+print("{} is {}".format(number, polarity))
