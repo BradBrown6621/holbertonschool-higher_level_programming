b6bbde441da5ef52a07fb3e8b1f3aa25c05020c6
BradBrown6621 <6621@holbertonstudents.com>
2023-09-19 11:29:48 -0700

    Added 2- and shebangs

---
+++ python-classes/2-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+class Square:
+
+    def __init__(self, size=0):
+        try:
+            size * 1
+            size >= 0
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            print("size must be an integer")
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
