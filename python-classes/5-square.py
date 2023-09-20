8045172545d5c0884d966b84a33f7c4210da40cd
BradBrown6621 <6621@holbertonstudents.com>
2023-09-20 08:05:48 -0700

    Added 5-

---
+++ python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Defines a square with data and methods
+"""
+
+
+class Square:
+    """Basic Square"""
+    def __init__(self, size=0):
+        """Defines initial square params"""
+        self.size(size)
+
+    def size(self):
+        """Returns the size of Square"""
+        return self.size
+
+    def size(self, value):
+        """Sets the size of square"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Returns the area of a square"""
+        try:
+            if self.size >= 0:
+                return self.size ** 2
+            else:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(0, self.size):
+                for j in range(0, self.size):
+                    print("#", end="")
+                print("")
