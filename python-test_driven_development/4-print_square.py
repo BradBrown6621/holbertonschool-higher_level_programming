f09d4794faf2fdaf506a612b0b69ab8f5abe4263
BradBrown6621 <6621@holbertonstudents.com>
2023-09-20 14:16:53 -0700

    Added 4- and test

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for printing a square of #"""
+
+def print_square(size):
+    if isinstance(size, int):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    elif isinstance(size, float):
+        if size < 0:
+            raise TypeError("size must be an integer")
+    else:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
