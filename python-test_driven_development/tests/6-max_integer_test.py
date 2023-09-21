6e510640df6e425e58fe1baab24912134ba87868
BradBrown6621 <6621@holbertonstudents.com>
2023-09-20 21:15:41 -0700

    Added 6-

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_maxintend(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+
+    def test_maxintstart(self):
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+
+    def test_negatives(self):
+        self.assertEqual(max_integer([3, 2, -1]), 3)
+
+    def test_morenegatives(self):
+        self.assertEqual(max_integer([-3, -2, -1]), -1)
+
+    def test_singleton(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
