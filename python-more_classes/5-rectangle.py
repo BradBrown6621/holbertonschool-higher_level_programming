30729fecd243bdd2fd813a84dcd7e5bab736e1df
BradBrown6621 <6621@holbertonstudents.com>
2023-09-20 20:07:00 -0700

    Added 5-

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Module that defines a rectangle"""
+
+
+class Rectangle():
+    """A definition of a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes instance of Rectangle"""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets Rectangle.width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets Rectangle.width to int"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets Rectangle.height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets Rectangle.height to int"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Returns string representation of Rectangle instance"""
+        if self.width == 0 or self.height == 0:
+            return ""
+
+        string = ""
+        for i in range(self.height):
+            string = string + "#" * self.width + "\n"
+
+        return string[:-1]
+
+    def __repr__(self):
+        """
+        Returns a string representation of Rectangle used by eval()
+        """
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        """
+        Performs functions when deleted
+        """
+        print("Bye rectangle...")
