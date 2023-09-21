2393357f30233240c39a50190b85acda10e3f4ee
BradBrown6621 <6621@holbertonstudents.com>
2023-09-20 18:46:34 -0700

    hotfix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,31 +7,38 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """Initializes instance of Rectangle"""
-        self.__width = width
-        self.__height = height
 
+        self.width = width
+        self.height = height
+
+    @property
     def width(self):
         """Gets Rectangle.width"""
+
         return self.__width
 
+    @width.setter
     def width(self, value):
         """Sets Rectangle.width to int"""
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
+    @property
     def height(self):
         """Gets Rectangle.height"""
+
         return self.__height
 
+    @height.setter
     def height(self, value):
         """Sets Rectangle.height to int"""
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
