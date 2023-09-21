c16c1c679c62f370cf9ddf2ffbc15c6ca8b9eb63
BradBrown6621 <6621@holbertonstudents.com>
2023-09-20 21:33:37 -0700

    fixed 2-

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if not my_list:
+        return []
     uniqs = []
     for target in my_list:
         if target not in uniqs:
             uniqs.append(target)
-    return uniqs
+    return sum(uniqs)
