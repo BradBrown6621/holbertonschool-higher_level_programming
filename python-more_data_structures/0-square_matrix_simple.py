a5663559803b1e19aa61d1e8f2657f33940f2b02
BradBrown6621 <6621@holbertonstudents.com>
2023-09-20 21:26:06 -0700

    pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Squares a matrix"""
+
+
 def square_matrix_simple(matrix=[]):
     """Squares a matrix"""
     if not matrix:
@@ -10,5 +12,4 @@ def square_matrix_simple(matrix=[]):
         square_matrix.append([])
         for cols in range(len(matrix[rows])):
             square_matrix[rows].append(matrix[rows][cols] ** 2)
-    
     return square_matrix
