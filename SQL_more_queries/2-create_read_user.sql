2ffea776de691990f856da75c6af130b7f0ffb56
BradBrown6621 <6621@holbertonstudents.com>
2023-10-24 20:24:02 -0700

    Added 2-

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- 2-create_read_user.sql
+-- Brad Brown
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT on *.* TO 'user_0d_2'@'localhost';
