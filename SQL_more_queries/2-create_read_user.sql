177503c840067b77fff3f0b08c5d96189713e2fd
BradBrown6621 <6621@holbertonstudents.com>
2023-10-24 20:31:01 -0700

    Properly working 2-

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- Brad Brown
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT on *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
